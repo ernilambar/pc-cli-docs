618c304a11fd79eee294ee2f6f9300bb024e483e
Nilambar Sharma <nilambar@outlook.com>
2024-01-21 14:08:07 +0545

    Add plugin file

---
+++ pc-cli-docs.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: PC CLI Docs
+ * Description: PC CLI Docs.
+ * Requires at least: 6.3
+ * Requires PHP: 7.0
+ * Version: 0.1.0
+ * Author: WordPress Performance Team
+ * Author URI: https://make.wordpress.org/performance/
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: pc-cli-docs
+ *
+ * @package pc-cli-docs
+ */
